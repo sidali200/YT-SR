ccea0289ea473822872c2bfc453d6ba8f90f4207
sidali200 <53345569+sidali200@users.noreply.github.com>
2019-07-26 17:56:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('message', message => {
   if (message.content.startsWith(prefix + 'setw')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`Watch Now: **${argresult}`)
+    message.channel.sendMessage(`-:white_check_mark:-`)
 } 
 
  
